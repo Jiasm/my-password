be0d89d53310f634ed7116ab7daebe32d15cc619
jiasm <jiashunming@outlook.com>
2016-08-29 14:47:01 +0800

    update

---
+++ bin/index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+'use strict'
+
+const crypto = require('crypto')
+const key = '666666'
+const cipher = crypto.createCipher('aes192', key);
+const decipher = crypto.createDecipher('aes192', key);
+
+let {2: type, 3: password} = process.argv
+
+let pwd = 1
+
+switch (type) {
+  case 'encode':
+    // 加密
+    pwd = cipher.update(password, 'utf8', 'hex')
+    pwd += cipher.final('hex')
+
+    console.log(`[\x1B[33m${password}\x1B[39m]加密后的密码为：`)
+    console.log(`\x1B[31m${pwd}\x1B[39m`)
+    break
+  case 'decode':
+    // 解密
+    pwd = decipher.update(password, 'hex', 'utf8')
+    pwd += decipher.final('utf8')    //  new Buffer(`${crypto.createHash('MD5').update(password, 'UTF-8').digest('hex')}`).toString('base64')
+
+    console.log(`[\x1B[31m${password}\x1B[39m]解密后的密码为：`)
+    console.log(`\x1B[33m${pwd}\x1B[39m`)
+    break
+}
